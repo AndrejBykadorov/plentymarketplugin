ea7a84bb206e0915dbf1933155481b473d690399
Andrej Bykadorov <andrej.bykadorov@gmail.com>
2019-07-03 09:41:18 +0200

    Adding php ui

---
+++ src/index.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+<head>
+    <title>Ecos</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <base href="./">
+</head>
+<body>
+Hello ECOS
+<br/>
+<?php echo $_GET["action"]; ?>
+</body>
+</html>
