55fed85314fba542b90914b90398cd4c916d7013
Andrej Bykadorov <andrej.bykadorov@gmail.com>
2019-07-08 16:25:13 +0200

    Return request - fix nix VIII

--- src/Controllers/AuthController.php
+++ src/Controllers/AuthController.php
@@ -29,7 +29,7 @@ class AuthController extends Controller
 
     public function getAccessToken(Request $request, Response $response){
             if(is_null($this->contract->getCurrentAuthorizedUser())){
-                return $response->json($this->contract);
+                return $response->json(serialize($this->contract));
             }else{
                 return $response->json(["else" => "else"]);
             }
