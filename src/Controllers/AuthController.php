b7109830c7bda14f9055f2892dbfa72723cb24a4
Andrej Bykadorov <andrej.bykadorov@gmail.com>
2019-07-09 16:14:06 +0200

    Return request - fix nix 6

--- src/Controllers/AuthController.php
+++ src/Controllers/AuthController.php
@@ -23,60 +23,19 @@ class AuthController extends Controller
 
 {
 
-    const PLUGIN_NAME = 'Etsy';
-    const TABLE_NAME = 'settings';
-    const SETTINGS_TOKEN_REQUEST = 'token_request';
-    const SETTINGS_ACCESS_TOKEN = 'access_token';
-    const SETTINGS_SETTINGS = 'settings';
-    const SETTINGS_ETSY_SHOPS = 'etsy_shops';
-    const SETTINGS_ORDER_REFERRER = 'order_referrer';
-    const SETTINGS_LAST_ORDER_IMPORT = "last_order_import";
-    const SETTINGS_LAST_ITEM_EXPORT = "last_item_export";
-    const SETTINGS_LAST_STOCK_UPDATE = "last_stock_update";
-    const SETTINGS_PROCESS_ITEM_EXPORT = 'item_export';
-    const SETTINGS_PROCESS_STOCK_UPDATE = 'stock_update';
-    const SETTINGS_PROCESS_ORDER_IMPORT = 'order_import';
 
-    private $dynamoDbRepo;
-    private $item;
-    private $imageRepository;
-    private $cred;
+private $settings;
 
-
-    public function __construct(DynamoDbRepositoryContract $dynamoDbRepository)
+    public function __construct(\Plenty\Modules\System\Contracts\WebstoreConfigurationRepositoryContract $settings)
     {
-        $this->dynamoDbRepo = $dynamoDbRepository;
+        $this->settings = $settings;
     }
-
-    public function get($name, $default = null)
-    {
-        $data = $this->dynamoDbRepo->getItem("Etsy", self::TABLE_NAME, true, [
-            'name' => [\Plenty\Modules\Plugin\DynamoDb\Contracts\DynamoDbRepositoryContract::FIELD_TYPE_STRING => $name]
-        ]);
-        if(isset($data['value'][ \Plenty\Modules\Plugin\DynamoDb\Contracts\DynamoDbRepositoryContract::FIELD_TYPE_STRING ]))
-        {
-            return $data['value'][ \Plenty\Modules\Plugin\DynamoDb\Contracts\DynamoDbRepositoryContract::FIELD_TYPE_STRING ];
-        }
-        return $default;
-    }
-
 
 
     public function getAccessToken(Request $request, Response $response){
 
 
-        //$this->cred->all();
-
-
-        //$cred =  pluginApp(\Plenty\Modules\Market\Credentials\Contracts\CredentialsRepositoryContract::class);
-
-
-
-
-        $data = $this->get(self::SETTINGS_ACCESS_TOKEN);
-
-
-        //$tokenData = $this->accountHelper->getTokenData();
+        $data = $this->settings->findByPlentyId(44475)->toArray();
 
 
         return $response->json($data);
