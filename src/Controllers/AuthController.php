edeb64387d6b676b16d0f0b0abe82c442c9ebd82
Andrej Bykadorov <andrej.bykadorov@gmail.com>
2019-07-09 16:59:43 +0200

    Return request - fix nix 6

--- src/Controllers/AuthController.php
+++ src/Controllers/AuthController.php
@@ -42,11 +42,9 @@ class AuthController extends Controller
 
         $itemRep = pluginApp(\Plenty\Modules\Item\Item\Contracts\ItemRepositoryContract::class);
 
-        $params = [
-                'images' => null
-        ];
+        $params = ["images"];
 
-        $item_list = $itemRep->search([],[],1,50)->toArray();
+        $item_list = $itemRep->search([],[],1,50, $params)->toArray();
 
         foreach ($item_list["entries"] as $key => $item_array) {
 
