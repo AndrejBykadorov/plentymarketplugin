3681dfb3f5cc3ba10110de57cd38c665300850d1
Andrej Bykadorov <andrej.bykadorov@gmail.com>
2019-07-19 13:38:37 +0200

    Return request - fix nix 6

--- src/Controllers/ItemsController.php
+++ src/Controllers/ItemsController.php
@@ -30,9 +30,134 @@ class ItemsController extends Controller
 
         $params = ["itemImages"];
 
-        $item_list = $this->item->search(null, [], $params)->toArray();
+        $item_list = $this->item->search($this->resultFields(), [], $params)->toArray();
 
         return $response->json($item_list);
+    }
+
+
+    private function resultFields()
+    {
+        $resultFields = [
+            'itemBase' => [
+                'id',
+                'producer',
+            ],
+            'itemShippingProfilesList' => [
+                'id',
+                'name',
+            ],
+            'itemDescription' => [
+                'params' => $this->getItemDescriptionParams(),
+                'fields' => [
+                    'name1',
+                    'description',
+                    'shortDescription',
+                    'technicalData',
+                    'keywords',
+                    'lang',
+                ],
+            ],
+            'variationMarketStatus' => [
+                'params' => [
+                    'marketId' => ""
+                ],
+                'fields' => [
+                    'id',
+                    'sku',
+                    'marketStatus',
+                    'additionalInformation',
+                ]
+            ],
+            'variationBase' => [
+                'id',
+                'limitOrderByStockSelect',
+                'weightG',
+                'lengthMm',
+                'widthMm',
+                'heightMm',
+                'attributeValueSetId',
+            ],
+            'variationRetailPrice' => [
+                'price',
+                'currency',
+            ],
+            'variationStock' => [
+                'params' => [
+                    'type' => 'virtual'
+                ],
+                'fields' => [
+                    'stockNet'
+                ]
+            ],
+            'variationStandardCategory' => [
+                'params' => [
+                    'plentyId' => '44475',
+                ],
+                'fields' => [
+                    'categoryId'
+                ],
+            ],
+            'itemCharacterList' => [
+                'itemCharacterId',
+                'characterId',
+                'characterValue',
+                'characterValueType',
+                'isOrderCharacter',
+                'characterOrderMarkup'
+            ],
+            'variationAttributeValueList' => [
+                'attributeId',
+                'attributeValueId'
+            ],
+            'variationImageList' => [
+                'params' => [
+                    'all_images'                                       => [
+                        'type'                 => 'all', // all images
+                        'fileType'             => ['gif', 'jpeg', 'jpg', 'png'],
+                        'imageType'            => ['internal'],
+
+                    ],
+                    'only_current_variation_images_and_generic_images' => [
+                        'type'                 => 'item_variation', // current variation + item images
+                        'fileType'             => ['gif', 'jpeg', 'jpg', 'png'],
+                        'imageType'            => ['internal'],
+
+                    ],
+                    'only_current_variation_images'                    => [
+                        'type'                 => 'variation', // current variation images
+                        'fileType'             => ['gif', 'jpeg', 'jpg', 'png'],
+                        'imageType'            => ['internal'],
+
+                    ],
+                    'only_generic_images'                              => [
+                        'type'                 => 'item', // only item images
+                        'fileType'             => ['gif', 'jpeg', 'jpg', 'png'],
+                        'imageType'            => ['internal'],
+
+                    ],
+                ],
+                'fields' => [
+                    'imageId',
+                    'type',
+                    'fileType',
+                    'path',
+                    'position',
+                    'attributeValueId',
+                ],
+            ],
+        ];
+        return $resultFields;
+    }
+
+    private function getItemDescriptionParams()
+    {
+
+            $list[ "de" ] = [
+                'language' => "de",
+            ];
+
+        return $list;
     }
 }
 
